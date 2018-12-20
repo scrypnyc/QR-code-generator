d0d562983662681602b94078fe55054aaab99171
scrypnyc <alexanderlookua@gmail.com>
2018-12-20 17:11:54 +0200

    v.1.0

--- QRcode/ViewController.swift
+++ QRcode/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var dataInputField: UITextField!
+    @IBOutlet weak var containerForQRImage: UIImageView!
+    let filterForQRCode = "CIQRCodeGenerator"
+    let keyInputMessage = "inputMessage"
+
+
+
+    @IBAction func generateQR(_: UIButton) {
+    if let textToConvert = dataInputField.text?.data(using: String.Encoding.ascii) {
+            print("blabla")
+            let filter = CIFilter(name: filterForQRCode)
+            filter?.setValue(textToConvert, forKey: keyInputMessage)
+            
+        let myQRImage = UIImage(ciImage: (filter?.outputImage)!)
+            containerForQRImage.image = myQRImage
+    } else {
+        print("bla bla")
+        }
     }
 
-
 }
-
