61c369e92f19aae689eb9bfb0791550d7736eee5
scrypnyc <alexanderlookua@gmail.com>
2018-12-20 11:24:46 +0200

    Initial Commit

---
+++ QRcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRcode
+//
+//  Created by Alexander Skrypnyk on 12/20/18.
+//  Copyright © 2018 AlexanderSkrypnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
